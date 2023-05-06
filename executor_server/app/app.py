ccecbf4e2de3c7c9313ffb6702b93f08e0aed549
ashevlyakov <shevlyakov.tlkpi@gmail.com>
2023-05-07 01:50:36 +0300

    add execution server code run logic

--- executor_server/app/app.py
+++ executor_server/app/app.py
@@ -1,13 +1,10 @@
 from flask import Flask
-from flask_cors import CORS
 from api.code_blueprint import code_blueprint
 
 app = Flask(__name__)
 
-# TODO add restrict polic
-CORS(app)
 
 app.register_blueprint(code_blueprint, url_prefix='/code')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', port=8000, debug=True)
